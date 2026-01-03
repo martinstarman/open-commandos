14521a9e336e5f4abff33adc057841c29c54289f
martinstarman <martin@goodsailors.com>
2026-01-03 23:07:59 +0100

    chore: simplify build

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 g++                          \
   -o build/opencommandos.exe \
-  main.cpp                   \
-  utils/bmp_file.cpp         \
-  utils/dir_file.cpp         \
-  utils/rle_file.cpp         \
-  utils/utils.cpp            \
-  utils/wad_file.cpp         \
+  src/main.cpp               \
+  src/utils/bmp_file.cpp     \
+  src/utils/dir_file.cpp     \
+  src/utils/rle_file.cpp     \
+  src/utils/utils.cpp        \
+  src/utils/wad_file.cpp     \
   -I include                 \
-  -I utils                   \
   -L lib                     \
-  -l raylib                  \
-  -l gdi32                   \
-  -l winmm
+  -lraylib                   \
+  -lgdi32                    \
+  -lwinmm
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # open-commandos
+
+## Building
+See [raylib wiki](https://github.com/raysan5/raylib/wiki) and [raygui](https://github.com/raysan5/raygui) for more info. 
